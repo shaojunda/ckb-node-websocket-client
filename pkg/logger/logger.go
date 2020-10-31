724b951e98c26267a5a06a71b481ac7090994458
shaojunda <shaojunda@gmail.com>
2020-10-31 22:55:53 +0800

    feat: add log level

---
+++ pkg/logger/logger.go
@@ -0,0 +1,30 @@
+package logger
+
+type Level int
+
+const (
+	LevelDebug Level = iota
+	LevelInfo
+	LevelWarn
+	LevelError
+	LevelFatal
+	LevelPanic
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelDebug:
+		return "debug"
+	case LevelInfo:
+		return "info"
+	case LevelWarn:
+		return "warn"
+	case LevelError:
+		return "error"
+	case LevelFatal:
+		return "fatal"
+	case LevelPanic:
+		return "panic"
+	}
+	return ""
+}
