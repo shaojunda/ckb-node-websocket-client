55c3c3caf525cf3be2a94e726f0a080a76b36db2
shaojunda <shaojunda@gmail.com>
2020-11-03 01:55:03 +0800

    feat: add poolTransactionEntry service

---
+++ internal/service/pool_transaction_entry.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/shaojunda/ckb-node-websocket-client/internal/rpc"
+)
+
+func (svc *Service) CreatePoolTransactionEntry(entry rpc.PoolTransactionEntry, fee hexutil.Uint64, cycles hexutil.Uint64, size hexutil.Uint64) error {
+	entry.Fee = fee
+	entry.Cycles = cycles
+	entry.Size = size
+	poolTx, err := entry.ToPoolTransactionEntryModel()
+	if err != nil {
+		return err
+	}
+	_, err = svc.dao.CreatePoolTransactionEntry(&poolTx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
