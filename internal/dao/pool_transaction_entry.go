82bfeb8533f84ce7fa406b014cbd8fa161bd1ca1
shaojunda <shaojunda@gmail.com>
2020-11-03 01:54:32 +0800

    feat: add pollTransactionEntry dao

---
+++ internal/dao/pool_transaction_entry.go
@@ -0,0 +1,26 @@
+package dao
+
+import (
+	"github.com/shaojunda/ckb-node-websocket-client/internal/model"
+)
+
+func (d Dao) CreatePoolTransactionEntry(param *model.PoolTransactionEntry) (*model.PoolTransactionEntry, error) {
+	poolTx := model.PoolTransactionEntry{
+		CellDeps:       param.CellDeps,
+		TxHash:         param.TxHash,
+		HeaderDeps:     param.HeaderDeps,
+		Inputs:         param.Inputs,
+		Outputs:        param.Outputs,
+		OutputsData:    param.OutputsData,
+		Version:        param.Version,
+		Witnesses:      param.Witnesses,
+		TransactionFee: param.TransactionFee,
+		BlockNumber:    param.BlockNumber,
+		BlockTimestamp: param.BlockTimestamp,
+		Cycles:         param.Cycles,
+		TxSize:         param.TxSize,
+		DisplayInputs:  param.DisplayInputs,
+		DisplayOutputs: param.DisplayOutputs,
+	}
+	return poolTx.Create(d.engine)
+}
