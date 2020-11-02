6f34d9f8866f0f6c81f38687660dff08a7f235d1
shaojunda <shaojunda@gmail.com>
2020-11-03 01:52:27 +0800

    chore: move chain types to model

--- internal/model/model.go
+++ internal/model/model.go
@@ -36,3 +36,33 @@ func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
 
 	return db, nil
 }
+
+type DepType string
+type ScriptHashType string
+
+type CellDep struct {
+	OutPoint OutPoint `json:"out_point"`
+	DepType  DepType  `json:"dep_type"`
+}
+
+type OutPoint struct {
+	TxHash Hash `json:"tx_hash"`
+	Index  uint `json:"index"`
+}
+
+type Script struct {
+	CodeHash Hash           `json:"code_hash"`
+	HashType ScriptHashType `json:"hash_type"`
+	Args     string         `json:"args"`
+}
+
+type CellInput struct {
+	Since          uint64   `json:"since"`
+	PreviousOutput OutPoint `json:"previous_output"`
+}
+
+type CellOutput struct {
+	Capacity uint64  `json:"capacity"`
+	Lock     *Script `json:"lock"`
+	Type     *Script `json:"type"`
+}
